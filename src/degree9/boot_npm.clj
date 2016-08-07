1c5e6fc625e2db163c0c1c6150570f7bb036c039
Matthew Ratzke <matt@degree9.io>
2016-08-07 14:14:33 -0600

    added exec task

--- src/degree9/boot_npm.clj
+++ src/degree9/boot_npm.clj
@@ -1,5 +1,4 @@
 (ns degree9.boot-npm
-  {:boot/export-tasks true}
   (:require [boot.core :as boot]
             [boot.tmpdir :as tmpd]
             [boot.util :as util]
@@ -14,7 +13,7 @@
    i install     FOO=BAR {kw str} "Dependency map."
    d development         bool     "Include development dependencies with packages."
    r dry-run             bool     "Report what changes npm would have made. (usefull with boot -vv)"
-   g global              bool     "Opperates in global mode. Packages are installed to "
+   g global              bool     "Opperates in global mode. Packages are installed to prefix."
    c cache-key   VAL     kw       "Optional cache key for when npm is used with multiple dependency sets."]
   (let [npmjsonf  (:npm         *opts* "./package.json")
         deps      (:install     *opts*)
@@ -24,11 +23,31 @@
         tmp       (boot/cache-dir! cache-key)
         tmp-path  (.getAbsolutePath tmp)
         npmjson   (generate-string {:name "boot-npm" :version "0.1.0" :dependencies deps})
-        args      ["install"]
-        args      (cond-> args
+        args      (cond-> ["install"]
                     (not dev) (conj "--production")
                     dry-run   (conj "--dry-run")
                     global    (conj "--global"))]
     (comp
       (exec/properties :contents npmjson :directory tmp-path :file npmjsonf)
       (exec/exec :process "npm" :arguments args :directory tmp-path :local "bin"))))
+
+(boot/deftask exec
+  "Exec wrapper for npm modules"
+  [m module         VAL  str      "NPM node module."
+   p process        VAL  str      "CLI executable of the npm module. (if different than module name)"
+   d version        VAL  str      "Module version string."
+   a arguments      VAL  [str]    "List of arguments to pass to cli process."
+   g global              bool     "Opperates in global mode. Packages are installed to global location."
+   c cache-key      VAL  kw       "Optional cache key for when npm is used with multiple dependency sets."]
+  (let [module    (:module    *opts*)
+        process   (:process   *opts* module)
+        version   (:version   *opts* "*")
+        args      (:arguments *opts*)
+        global    (:global    *opts*)
+        cache-key (:cache-key *opts* ::cache)
+        install   (assoc {} (keyword module) version)
+        tmp       (boot/cache-dir! cache-key)
+        tmp-path  (.getAbsolutePath tmp)]
+    (comp
+      (npm :install install :cache-key cache-key :global global)
+      (exec/exec :process process :arguments args :cache-key cache-key :local (str "node_modules/" module "/bin")))))
